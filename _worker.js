ce4857612751b0e7e041dd9fcf305780633a9b1a
Martin <qule520@126.com>
2024-09-19 19:39:00 +0800

    feat: 重构转发逻辑，支持多路由

--- _worker.js
+++ _worker.js
@@ -1,14 +1,22 @@
 export default {
 	async fetch(request, env, ctx) {
-		let target = env.TARGET ?? "";
-		if (!target.startsWith("http")) {
-			return new Response("Not Found", { status: 404 });
-		}
-		if (target.endsWith("/")) {
-			target = target.substring(0, target.length - 1);
-		}
+		let routes = env.ROUTES ? env.ROUTES.split(",") : [];
+		// 解析成 [[source,target]..]这样的二维数组
+		routes = routes.map(i => i.split("="));
 		const url = new URL(request.url);
-		const newReq = new Request(target + url.pathname + url.search, { body: request.body, method: request.method, headers: request.headers, redirect: "follow" });
-		return fetch(newReq);
+		for (let [source, target] of routes) {
+			if (source == url.hostname) {
+				if (!target.match(/^https?:\/\//)) {
+					target = "https://" + target;
+				}
+				if (target.endsWith("/")) {
+					target = target.substring(0, target.length - 1);
+				}
+				console.log(`FORWARD ${request.method} ${source} => ${target}`);
+				const newReq = new Request(target + url.pathname + url.search, { body: request.body, method: request.method, headers: request.headers, redirect: "follow" });
+				return fetch(newReq);
+			}
+		}
+		return new Response("Not Found.", { status: 404 });
 	},
 };
